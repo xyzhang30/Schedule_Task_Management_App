4b163e812c67160b7437b4979cb3bee75ad6efee
oliviafu <qf35@duke.edu>
2024-10-05 18:14:29 -0400

    #2 revise landing page

--- frontend/src/landingPage/landingPage.js
+++ frontend/src/landingPage/landingPage.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
         <h1>Master Your College Life</h1>
         <p>Organize your academic and personal life with ease.</p>
         <div className="cta-buttons">
-          <button className="btn get-started">Get Started</button>
+          <button className="btn sign-up">Sign Up</button>
           <button className="btn log-in">Log In</button>
         </div>
       </section>
