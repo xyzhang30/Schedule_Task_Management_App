4ec72e4a197711920a34d92a504b6c734539d26a
Alisha Zhang <xz352@duke.edu>
2024-10-08 16:22:27 -0400

    #10, #9 flask + database setup + docker running

--- backend/models/account.py
+++ backend/models/account.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 from ..db import Base, db_session
-# from .artist import Artist
 
 class Account(Base):
       __tablename__ = 'Account'
