72c1eb5a6582a49a3a5ae6012c57347b4cad41e8
Alisha Zhang <xz352@duke.edu>
2024-10-10 18:10:10 -0400

    #10, #9: backend db connection fully working

--- backend/controllers/accountController.py
+++ backend/controllers/accountController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, render_template
 from flask import request
 from ..models.account import Account
 
@@ -6,18 +6,37 @@ bp = Blueprint('account', __name__, url_prefix='/account')
 
 @bp.route('/', methods = ['GET'])
 def index():
-    print("Fetching all accounts")
-    try:
-        return "All accounts!"
-    except Exception as e:
-        print(f"Error: {e}")
-        return "An error occurred", 500
+    account = Account.query.all()
+    print(account)
+    accounts_list = [a.to_dict() for a in account]
+    return jsonify(accounts_list)
+
+
+@bp.route('/create', methods = ['POST'])
+def createAccount():
+    name = request.form.get("name")
+    print("request!!!!!!: ", request.form)
+    # id = request.form.get('id')
+    account = Account(name=name)
+    account.save()
+    return index()
+
+
+  # return "hi"
+    # print("Fetching all accounts")
+    # try:
+    #     return "All accounts!"
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    #     return "An error occurred", 500
 #   return "all accounts!"
 
-@bp.route('/<id>', methods = ['GET'])
-def show(id):
-  account = Account.query.get(id)
-  if account:
-    return account.name
-  else:
-    return "Account doesn't exist"
+# @bp.route('/<id>', methods = ['GET'])
+# def show(id):
+#   account = Account.query.get(id)
+#   if account:
+#     return account.name
+#   else:
+#     return "Account doesn't exist"
+
+
--- backend/models/account.py
+++ backend/models/account.py
@@ -4,5 +4,20 @@ from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 from ..db import Base, db_session
 
 class Account(Base):
-      __tablename__ = 'Account'
-      
+	__tablename__ = 'Accounts'
+	id = Column(Integer, primary_key=True)
+	name = Column(String, unique=False)
+
+	def __repr__(self):
+		return f"<Account id={self.id} name={self.name}>"
+
+	@classmethod
+	def all(cls):
+		return db_session.query(cls).all()
+
+	def save(self):
+		db_session.add(self)
+		db_session.commit()
+
+	def to_dict(self):
+		return {column.name: getattr(self, column.name) for column in self.__table__.columns}
