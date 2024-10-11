387c13d80e973ecb4a3d85aa409c54f2643e4b15
DavidShenkerman <dls132@duke.edu>
2024-10-10 21:07:50 -0400

    Added password hashing and salting

--- backend/controllers/authController.py
+++ backend/controllers/authController.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, session, request, jsonify
+import bcrypt
 
 bp = Blueprint('auth', __name__, url_prefix = '/auth')
 
 @bp.route('/login', methods = ['POST'])
-
 def login():
     print("Logging in...")
     #gets user input
     user_inputted_username = request.form['username']
     user_inputted_password = request.form['password']
-    db = True #temporary pseudo variable, will be replaced with actual db query
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(user_inputted_password.encode('utf-8'), salt)
+    db = True #temporary pseudo variable, will be replaced with actual db query using user_inputted_username and hashed_password
     response_message = {'msg': 'Successfully Logged In'}
     status_code = 200
     if not db: 
