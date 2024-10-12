db86cfd2a3ad1217f0b9c707c9a4e804ebaffea0
Alisha Zhang <xz352@duke.edu>
2024-10-11 20:53:37 -0400

    #11: create table script

--- backend/models/account.py
+++ backend/models/account.py
@@ -4,12 +4,17 @@ from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 from ..db import Base, db_session
 
 class Account(Base):
-	__tablename__ = 'Accounts'
-	id = Column(Integer, primary_key=True)
-	name = Column(String, unique=False)
+	__tablename__ = 'accounts'
+	account_id = Column(Integer, primary_key=True)
+	username = Column(String, unique=True)
+	password = Column(String, unique=False)
+	email = Column(String, unique=True)
+	phone = Column(String, unique=True)
+	avatar = Column(String, unique=False)
+	year_created = Column(Integer, unique=False)
 
-	def __repr__(self):
-		return f"<Account id={self.id} name={self.name}>"
+	# def __repr__(self):
+		# return f"<Account id={self.id} name={self.name}>"
 
 	@classmethod
 	def all(cls):
