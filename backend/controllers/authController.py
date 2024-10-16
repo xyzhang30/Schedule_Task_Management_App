ecc87aecfe2bc1e97b63a518f898f26988f612e6
DavidShenkerman <dls132@duke.edu>
2024-10-16 19:16:06 -0400

    Integrated account model and added database functionality to login and register endpoints

--- backend/controllers/authController.py
+++ backend/controllers/authController.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, session, request, jsonify
 import bcrypt
 import uuid
+from ..models.account import Account
 
 bp = Blueprint('auth', __name__, url_prefix = '/auth')
 
@@ -14,19 +15,27 @@ def login():
     user_inputted_email = request.form['email']
     user_inputted_password = request.form['password']
 
-    db = True #temporary pseudo variable, will be replaced with actual db query using user_inputted_username and hashed_password
+    try:
+        account = Account.get_acc_by_username(user_inputted_username) #Check if their username exists and get the account tuple associated with it
+        salt = bcrypt.gensalt() #Generate salt for the user inputted password
+        hashed_password = bcrypt.hashpw(user_inputted_password.encode('utf-8'), salt) #Hash the password they entered
 
-    response_message = {'msg': 'Successfully Logged In'}
-    status_code = 200
+        if hashed_password == account.password: #Compare their password with the password in the database
+            response_message = {'msg': 'Successfully Logged In'}
+            status_code = 200
+            session['user'] = account.account_id   #keep track of logged in status with the account id
 
-    if not db: #Query was invalid i.e. bad credientials
+        else: #Case where password is incorrect
+            response_message = {'msg': 'Incorrect Username or Password'}
+            status_code = 401
+    
+        return jsonify(response_message), status_code
+    
+    except Exception as e: #Case where username does not exist
         response_message = {'msg': 'Incorrect Username or Password'}
         status_code = 401
-    else: 
-        session['user'] = request.form['username'] #keep track of logged in status - this will change to a database query to get the user's unique id
-
-    return jsonify(response_message), status_code
-
+        return jsonify(response_message), status_code
+    
 @bp.route('/register', methods = ['POST'])
 def register():
     print("Registering...")
@@ -36,21 +45,31 @@ def register():
     user_inputted_password = request.form['password']
     user_inputted_email = request.form['email']
     user_inputted_phone_number = request.form['phone_number']
-    user_inputted_year = request.form['year']
+    user_inputted_year_created = (int)(request.form['year'])
 
-    account_id = uuid.uuid1() #generate unique account id
-    salt = bcrypt.gensalt() #generate salt
-    hashed_password = bcrypt.hashpw(user_inputted_password.encode('utf-8'), salt) #Hash the password with the salt
+    generated_id = uuid.uuid1() #generate unique account id
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(user_inputted_password.encode('utf-8'), salt)
 
-    db = True #DB query to register, to be implemented soon
+    new_account = Account(
+        account_id = generated_id,
+        username = user_inputted_username,
+        password = user_inputted_password,
+        email = user_inputted_email,
+        phone = user_inputted_phone_number,
+        avatar = "0",
+        year_created = user_inputted_year_created
+    )
 
-    response_message = {'msg': 'Successfully Registered'}
-    status_code = 200
-
-    if not db: #query went wrong: they maybe used a username/email that wasn't unique
+    try:
+        new_account.save()
+        response_message = {'msg': 'Successfully Registered'}
+        status_code = 200
+        session['user'] = new_account.account_id 
+        return jsonify(response_message), status_code
+    
+    except Exception as e:
         response_message = {'msg': "Invalid Email or Username"} #Need to make this more fine tuned: they should know what field was bad. i.e. username already taken
         status_code = 409 #proper code for this type of issue apparently
-    else:
-        session['user'] = account_id #this effectively logs them in and keeps state with a unique identifier
-
-    return jsonify(response_message), status_code
+        return jsonify(response_message), status_code
+        
