d8301fd71cd8bda6dcb1d70c4c66d4f25dbd9579
DavidShenkerman <dls132@duke.edu>
2024-10-20 16:00:26 -0400

    Added auth functionality and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/landingPage.js';
 import Friends from './pages/Friends.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import Logout from './pages/Logout.js';
+import ChangeUsername from './pages/ChangeUsername.js';
+import ChangePassword from './pages/ChangePassword.js';
+import ChangePhoneNumber from './pages/ChangePhoneNumber.js';
 
 function App() {
   return (
-    // <div className="App">
-    //   <LandingPage />
-    // </div>
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/friends" element={<Friends/>} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/change-username" element={<ChangeUsername />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/change-phone-number" element={<ChangePhoneNumber />} />
       </Routes>
     </Router>
   );
