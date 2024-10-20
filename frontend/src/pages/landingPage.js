4452e937386a32a4ef721822e28623207de6cd8a
Alisha Zhang <xz352@duke.edu>
2024-10-20 16:55:50 -0400

    added css import to landing page

--- frontend/src/pages/landingPage.js
+++ frontend/src/pages/landingPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './landingPage.css';
 
 const LandingPage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
