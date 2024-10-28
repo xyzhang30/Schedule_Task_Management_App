e8c0c807116c3f06f2f38a0168b594b59b92efad
Alisha Zhang <xz352@duke.edu>
2024-10-28 11:25:53 -0400

    #12: removed user session hard coding

--- backend/controllers/friendController.py
+++ backend/controllers/friendController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 from flask import request
 from ..models.friend import Friend
 
@@ -11,11 +11,15 @@ def index():
     return jsonify(friend_list)
 
 
-@bp.route('/get-friends/<int:account_id>', methods=['GET'])
-def get_friends(account_id):
+@bp.route('/get-friends', methods=['GET'])
+def get_friends():
     '''
     Gets all the friends for a specific account by account_id
     '''
+
+    print("______: ", session)
+
+    account_id = session['user']
     friends = Friend.get_friends_by_id(account_id)
     if friends is None:
         return jsonify({"error": "No friends found."}), 404
@@ -29,7 +33,7 @@ def addFriend():
     '''
     add a friend account pair into database
     '''
-    account_id1 =  int(request.form.get("account_id1"))
+    account_id1 = session['user']
     account_id2 = int(request.form.get("account_id2"))
     # always save the smaller id as id1 -- ensures there are no bugs caused by the order in the friend pair
     if account_id1 > account_id2:
