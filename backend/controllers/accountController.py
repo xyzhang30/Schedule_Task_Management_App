a23e06b1663458e0e4be6f17daa46b16a66da546
DavidShenkerman <dls132@duke.edu>
2024-10-30 13:06:18 -0400

    Cleaned controller

--- backend/controllers/accountController.py
+++ backend/controllers/accountController.py
@@ -8,47 +8,62 @@ bp = Blueprint('account', __name__, url_prefix='/account')
 @bp.route('/change_username', methods = ['POST'])
 @is_logged_in
 def change_username():
+    '''
+    Changes a user's username
+    '''
     account = Account.get_acc_by_id(session['user'])
     new_username = request.form['new_username']
+
     try: 
         account.username = new_username
         account.save()
         response_message = {'msg': 'Succesfully changed username'}
         status_code = 201
-        return jsonify(response_message), status_code
     except Exception as e:
+        #Handle case where they did not enter a unique username
         response_message = {'msg': 'Username already taken'}
         status_code = 409
-        return jsonify(response_message), status_code
+        
+    return jsonify(response_message), status_code
     
 @bp.route('/change_email', methods = ['POST'])
 @is_logged_in
 def change_email():
+    '''
+    Changes a user's email
+    '''
     account = Account.get_acc_by_id(session['user'])
     new_email = request.form['new_email']
+
     try: 
         account.email = new_email
         account.save()
         response_message = {'msg': 'Succesfully changed email'}
         status_code = 201
-        return jsonify(response_message), status_code
     except Exception as e:
+        #Handle case where they did not enter a unique email
         response_message = {'msg': 'Email already taken'}
         status_code = 409
-        return jsonify(response_message), status_code
+
+    return jsonify(response_message), status_code
 
 @bp.route('/change_number', methods = ['POST'])
 @is_logged_in
 def change_number():
+    '''
+    Changes a user's phone number
+    '''
     account = Account.get_acc_by_id(session['user'])
     new_number = request.form['new_number']
+    
     try: 
         account.phone = new_number
         account.save()
         response_message = {'msg': 'Succesfully changed number'}
         status_code = 201
-        return jsonify(response_message), status_code
     except Exception as e:
+        #Handle case where they did not enter a unique phone number
         response_message = {'msg': 'Phone number already taken'}
         status_code = 409
-        return jsonify(response_message), status_code
+    
+    return jsonify(response_message), status_code
